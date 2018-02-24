c02178db55202319f6474357ad2a5e0b61ca8f0f
Sergio Alejandro Montoya Guevara <sergiomont94@gmail.com>
2018-02-23 21:47:06 -0600

    last Commit

--- ExamenPrg3/ExamenPrg3/main.cpp
+++ ExamenPrg3/ExamenPrg3/main.cpp
@@ -10,17 +10,17 @@ int main() {
 
 	Factura *lista = new Factura();
 	lista->agregarP(1, 100);
-	lista->buscar("Martin")->agregarProducto(2, "Leche", 50);
-	lista->buscar("Martin")->agregarProducto(2, "Manzana", 10);
+	lista->buscar("Dillion")->agregarProducto(2, "Leche", 50);
+	lista->buscar("Dillion")->agregarProducto(2, "Manzana", 10);
 
 	lista->Facturar(Fecha(23, 2, 2018), "Marin");
 	lista->Facturar(Fecha(23, 2, 2018), "Bryan");
-	lista->buscar("Nelson")->agregarP(2, "Sandia", 50);
-	lista->buscar("Nelson")->agregarP(2, "Pollo", 100);
-	lista->buscar("Nelson")->agregarP(2, "Melon", 60);
+	lista->buscar("Brenna")->agregarP(2, "Sandia", 50);
+	lista->buscar("Brenna")->agregarP(2, "Pollo", 100);
+	lista->buscar("Brenna")->agregarP(2, "Melon", 60);
 
 	
-	lista->buscar("Martin")->imprimirTotal();
+	lista->buscar("Sparks")->imprimirTotal();
 
 	cout << "Total de las facturas: " << lista->Facturar() << std::endl;
 	cout << "Total por todo: " << lista->total() << std::endl;
